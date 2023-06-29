78f81a616b5869f0fae702c84c3d62f2c214ed30
Luke Rogerson <luke@orbs.com>
2023-06-29 16:05:09 +0100

    Add check for minimum system requirements

--- installer/install.sh
+++ installer/install.sh
@@ -2,6 +2,7 @@
 
 # TODO
 # - Do we need sudo?
+# - Finalise minimum machine specs
 
 # Color variables
 RED='\033[0;31m'
@@ -17,10 +18,51 @@ NC='\033[0m' # No Color
 #     exit 1
 # fi
 
+# ----- Check minimum machine specs -----
+echo -e "${BLUE}Checking machine meets minimum hardware requirements...${NC}"
+# Min specs
+MIN_CPU=4   # in cores
+MIN_MEMORY=8   # in GB
+MIN_DISK=20 # in GB
+
+# Get current machine specs
+CPU=$(nproc --all)
+RAM=$(free -g | awk '/^Mem:/{print $2}')
+DISK=$(df -BG / | awk 'NR==2{print substr($4, 1, length($4)-1)}')
+
+# Check CPU
+if [ $CPU -lt $MIN_CPU ]; then
+    echo -e "${RED}Insufficient CPU cores. Required: $MIN_CPU, Available: $CPU.${NC}"
+    exit 1
+fi
+
+# Check RAM
+if [ $RAM -lt $MIN_MEMORY ]; then
+    echo -e "${RED}Insufficient memory. Required: $MIN_MEMORY GB, Available: $RAM GB."
+    exit 1
+fi
+
+# Check Disk
+if [ $DISK -lt $MIN_DISK ]; then
+    echo -e "${RED}Insufficient Disk space. Required: $MIN_DISK GB, Available: $DISK GB.${NC}"
+    exit 1
+fi
+
+echo -e "${GREEN}System meets minimum requirements!${NC}"
+echo "------------------------------------"
+
 #  ----- Install depedencies ----- 
-echo -e "${NC}Installing depencies...${NC}"
+echo -e "${BLUE}Installing dependencies...${NC}"
+
 apt-get update && apt-get -y upgrade
-apt-get install -y podman docker-compose
+apt-get install -y podman docker-compose git
 # Need to explicitly add docker.io registry
 echo "[registries.search]" > /etc/containers/registries.conf && echo "registries = ['docker.io']" >> /etc/containers/registries.conf
+
+echo -e "${GREEN}Finished installing dependencies!${NC}"
+echo "------------------------------------"
+
+# ----- Clone repo -----
+echo -e "${BLUE}Cloning repo...${NC}"
+# git clone 
 
